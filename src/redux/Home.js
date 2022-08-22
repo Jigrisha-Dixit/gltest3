a4af4cab36d315d7fe716562917156664fa2d405
Jigrisha-dixit <jigrisha.dixit@globallogic.com>
2022-08-22 12:30:07 +0530

    test3

---
+++ src/redux/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <h1>WELCOME TO MOVIE WORLD!!</h1>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/redux/Login.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Alert } from "react-bootstrap";
+import Home from "./Home";
+
+function Login() {
+  const [email, setEmail] = useState(" ");
+  const [password, setPassword] = useState(" ");
+
+  const [flag, setFlag] = useState(false);
+
+  const [home, setHome] = useState(true);
+
+  function handleLogin(e) {
+    e.preventDefault();
+    let pass = localStorage.getItem("Password").replace(/"/g, "");
+    let mail = localStorage.getItem("Email").replace(/"/g, "");
+
+    if (!email || !password) {
+      setFlag(true);
+      
+    } else if (password !== pass || email !== mail) {
+      setFlag(true);
+    } else {
+      setHome(!home);
+      setFlag(false);
+    }
+  }
+
+  return (
+    <div>
+      {home ? (
+        <form onSubmit={handleLogin}>
+          <h3>LogIn</h3>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="btn btn-dark btn-lg btn-block">
+            Login
+          </button>
+
+          {flag && (
+            <Alert color="primary" variant="warning">
+              Fill correct Info else keep trying.
+            </Alert>
+          )}
+        </form>
+      ) : (
+        <Home />
+      )}
+    </div>
+  );
+}
+
+export default Login;
